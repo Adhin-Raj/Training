f002d83d1f557d67e1fcdf25c3382fd6fa031ae4
Adhin Raj M <adhin.raj@vonnue.com>
2025-10-30 09:27:32 +0530

    feat: day -1 test case completed

--- DAY-1/Q.2/index.js
+++ DAY-1/Q.2/index.js
@@ -1,28 +1,95 @@
-
-
-const word="CAT"
-
-function patternPrint(word){
-    let i=0
-    while(i<word.length){
-        let combine="";
-        for(let j=0;j<= i;j++){
-            combine += word[j]
-        }
-        console.log(combine)
-        i++
+function patternPrint(word) {
+  let arr = [];
+  if (typeof word !== "string") return "input should be string";
+  let i = 0;
+  while (i < word.length) {
+    let combine = "";
+    for (let j = 0; j <= i; j++) {
+      combine += word[j];
     }
-     i=0
-    while(i<word.length-1){
-        let combined="";
-        for(let j=i+1;j<word.length;j++){
-            combined += word[j]
-        }
-        console.log(combined)
-        i++
+    // console.log(combine)
+    arr.push(combine);
+    i++;
+  }
+  i = 0;
+  while (i < word.length - 1) {
+    let combined = "";
+    for (let j = i + 1; j < word.length; j++) {
+      combined += word[j];
     }
-
+    // console.log(combined)
+    arr.push(combined);
+    i++;
+  }
+  return arr;
 }
 
-console.log(patternPrint(word))
+function patternPrintTest(arr) {
+  arr.forEach((element) => {
+    let flag = false;
+    const result = patternPrint(element[0]);
+    if (typeof result === "string" && result === element[1]) {
+      console.log("Passed");
+    } else {
+      for (let i = 0; i < element[1].length; i++) {
+        if (result[i] !== element[1][i]) {
+          console.log("Failed");
+          i = element[1].length;
+          flag = true;
+        }
+      }
 
+      if (!flag) {
+        console.log("Passed");
+        flag = false;
+      }
+    }
+  });
+
+  return `Completed...!`;
+}
+
+console.log(
+  patternPrintTest([
+    ["CAT", ["C", "CA", "CAT", "AT", "T"]],
+    ["DANCE", ["D", "DA", "DAN", "DANC", "DANCE", "ANCE", "NCE", "CE", "E"]],
+    ["123", ["1", "12", "123", "23", "3"]],
+    [123, "input should be string"],
+    ["!@#", ["!", "!@", "!@#", "@#", "#"]],
+    ["", []],
+    [true, "input should be string"],
+    [
+      "PROGRAMMING",
+      [
+        "P",
+        "PR",
+        "PRO",
+        "PROG",
+        "PROGR",
+        "PROGRA",
+        "PROGRAM",
+        "PROGRAMM",
+        "PROGRAMMI",
+        "PROGRAMMIN",
+        "PROGRAMMING",
+        "ROGRAMMING",
+        "OGRAMMING",
+        "GRAMMING",
+        "RAMMING",
+        "AMMING",
+        "MMING",
+        "MING",
+        "ING",
+        "NG",
+        "G",
+      ],
+    ],
+    [[], "input should be string"],
+    [{}, "input should be string"],
+    [["1"], "input should be string"],
+    [(item) => item, "input should be string"],
+    ["1a2b", ["1", "1a", "1a2", "1a2b", "a2b", "2b", "b"]],
+    ["a", ["a"]],
+    ["d-a", ["d", "d-", "d-a", "-a", "a"]],
+  ])
+);
